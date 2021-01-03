30ec9cd5ec865424c2c416f58b1211c093c9028c
kanrf_ <francisco@getxplor.com>
2021-01-02 21:59:25 -0500

    I have commented the auth

--- routes/datos_route/datos.js
+++ routes/datos_route/datos.js
@@ -5,9 +5,9 @@ const { decode } = require('../../middlewares/index')
 const { DatosController } = require('../../controllers/index');
 const route = express.Router();
 
-route.post('/datosrespo-create',  decode.autenticacion, DatosController.createDatosResponsable);
-route.get('/datosrespo-view/', decode.autenticacion, DatosController.viewMyDatosResponsable);
-route.get('/datosrespo-lists/', decode.autenticacion, DatosController.listsDatosResponsable);
-route.put('/datosrespo-update/:_id', decode.autenticacion, DatosController.updateDatosResponsable);
+route.post('/datosrespo-create', /*  decode.autenticacion, */ DatosController.createDatosResponsable);
+route.get('/datosrespo-view/',/*  decode.autenticacion, */ DatosController.viewMyDatosResponsable);
+route.get('/datosrespo-lists/',/*  decode.autenticacion, */ DatosController.listsDatosResponsable);
+route.put('/datosrespo-update/:_id',/*  decode.autenticacion, */ DatosController.updateDatosResponsable);
 
 module.exports = route
